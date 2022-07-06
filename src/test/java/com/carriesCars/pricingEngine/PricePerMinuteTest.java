53e7b2cdd9211ecd11d74b927a4155192b28edf4
Marijn Huizendveld <marijn@huizendveld.com>
2022-07-06 23:24:45 +0200

    :mag: `PricePerMinute`.

---
+++ src/test/java/com/carriesCars/pricingEngine/PricePerMinuteTest.java
@@ -0,0 +1,29 @@
+package com.carriesCars.pricingEngine;
+
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PricePerMinuteTest {
+
+    @Test
+    public void Multiplies_with_Duration() {
+        PricePerMinute pricePerMinute = new PricePerMinute(Money.of(CurrencyUnit.EUR, 0.24));
+        Duration someDuration = new Duration(8);
+
+        Money actual = pricePerMinute.calculateMinutePrice(someDuration);
+        Money expected = Money.of(CurrencyUnit.EUR, 1.92);
+        assertEquals(expected, actual);
+    }
+
+    private class PricePerMinute {
+        public PricePerMinute(Money pricePerMinute) {
+        }
+
+        public Money calculateMinutePrice(Duration someDuration) {
+            return null;
+        }
+    }
+}
