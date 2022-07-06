b0dee81b4d7d42e64290048e3fbbbbee175a67c2
Marijn Huizendveld <marijn@huizendveld.com>
2022-07-06 23:35:02 +0200

    :memo: Reorder code to focus on the important bits.

--- src/main/java/com/carriesCars/pricingEngine/PricePerMinute.java
+++ src/main/java/com/carriesCars/pricingEngine/PricePerMinute.java
@@ -3,13 +3,15 @@ package com.carriesCars.pricingEngine;
 import org.joda.money.Money;
 
 final class PricePerMinute {
+    public Money calculateMinutePrice(Duration someDuration) {
+        return pricePerMinute.multipliedBy(someDuration.durationInMinutes());
+    }
+
+    // Boring code below
+
     private Money pricePerMinute;
 
     public PricePerMinute(Money pricePerMinute) {
         this.pricePerMinute = pricePerMinute;
-    }
-
-    public Money calculateMinutePrice(Duration someDuration) {
-        return pricePerMinute.multipliedBy(someDuration.durationInMinutes());
     }
 }
