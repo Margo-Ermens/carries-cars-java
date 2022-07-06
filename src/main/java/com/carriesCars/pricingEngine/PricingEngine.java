170059b0ed4abeb8fbdc2417e9f8c95606d7e627
Marijn Huizendveld <marijn@huizendveld.com>
2022-07-06 23:33:50 +0200

    :package: Deprecate old API.

--- src/main/java/com/carriesCars/pricingEngine/PricingEngine.java
+++ src/main/java/com/carriesCars/pricingEngine/PricingEngine.java
@@ -3,6 +3,10 @@ package com.carriesCars.pricingEngine;
 import org.joda.money.Money;
 
 public class PricingEngine {
+    /**
+     * @deprecated More descriptive API available
+     * @see PricingEngine#calculatePriceV2
+     */
     public Money calculatePrice(Duration duration, Money pricePerMinute) {
         return calculatePriceV2(duration, new PricePerMinute(pricePerMinute));
     }
