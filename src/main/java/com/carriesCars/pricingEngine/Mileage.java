2e834cfec4184ca8ae0be7efcff6d2e4050a760b
Marijn Huizendveld <marijn@huizendveld.com>
2022-07-06 10:42:39 +0200

    :package: Make `Mileage` part of API.

---
+++ src/main/java/com/carriesCars/pricingEngine/Mileage.java
@@ -0,0 +1,24 @@
+package com.carriesCars.pricingEngine;
+
+class Mileage {
+    private int mileage;
+
+    public Mileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Mileage mileage1 = (Mileage) o;
+
+        return mileage == mileage1.mileage;
+    }
+
+    @Override
+    public int hashCode() {
+        return mileage;
+    }
+}
