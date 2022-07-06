81cc4bb6cd6716349eeb4834f64692be35854ff6
Marijn Huizendveld <marijn@huizendveld.com>
2022-07-06 23:40:49 +0200

    :construction: `PricePerMinute.inAmsterdam`

--- src/main/java/com/carriesCars/pricingEngine/PricePerMinute.java
+++ src/main/java/com/carriesCars/pricingEngine/PricePerMinute.java
@@ -1,5 +1,6 @@
 package com.carriesCars.pricingEngine;
 
+import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 
 final class PricePerMinute {
@@ -8,7 +9,7 @@ final class PricePerMinute {
     }
 
     public static PricePerMinute inAmsterdam() {
-        return null;
+        return new PricePerMinute(Money.of(CurrencyUnit.EUR, 0.24));
     }
 
     // Boring code below
