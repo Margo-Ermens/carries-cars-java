b00f91d587d4815b545e33d0d487e7684719f5b2
Marijn Huizendveld <marijn@huizendveld.com>
2022-07-06 10:44:42 +0200

    :smile: Parameterize test.

--- src/test/java/com/carriesCars/pricingEngine/MileageTest.java
+++ src/test/java/com/carriesCars/pricingEngine/MileageTest.java
@@ -1,18 +1,23 @@
 package com.carriesCars.pricingEngine;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MileageTest {
 
-    @Test
-    public void One_Mileage_is_identical_to_another_Mileage_if_they_have_an_identical_value() {
-        Mileage someMileage = new Mileage(250);
-        Mileage anotherIdenticalMileage = new Mileage(250);
+    @ParameterizedTest(name = "mileage=''{0}'' (example no. {index})")
+    @ValueSource(
+        ints = {
+            250
+        }
+    )
+    public void One_Mileage_is_identical_to_another_Mileage_if_they_have_an_identical_value(int aMileage) {
+        Mileage someMileage = new Mileage(aMileage);
+        Mileage anotherIdenticalMileage = new Mileage(aMileage);
 
         // Do you agree that this test should pass?
         assertEquals(someMileage, anotherIdenticalMileage);
     }
-
 }
