29982e9c4205ca44709187f708888388603556a8
YorrinK <yorrin.kerseboom@pharmapartners.nl>
2022-07-07 14:43:18 +0200

    hoi

--- src/main/java/com/carriesCars/pricingEngine/PricingEngine.java
+++ src/main/java/com/carriesCars/pricingEngine/PricingEngine.java
@@ -6,4 +6,25 @@ public class PricingEngine {
     public Money calculatePrice(Duration duration, Money pricePerMinute) {
         return pricePerMinute.multipliedBy(duration.durationInMinutes());
     }
+    public Money calculatePrice(Duration duration, Money pricePerMinute, Rental rental) {
+        Money totalPrice = pricePerMinute.multipliedBy(duration.durationInMinutes());
+        if(rental.getReservationDuration().durationInMinutes() - rental.getBaseMinutesToReach().durationInMinutes() > 0) {
+            Duration paidExtensionTime = Duration.ofMinutes(rental.getReservationDuration().durationInMinutes() - rental.getBaseMinutesToReach().durationInMinutes());
+            totalPrice = totalPrice.plus(rental.getExtensionCostPerMinute().multipliedBy(paidExtensionTime.durationInMinutes()));
+        }
+        return totalPrice;
+    }
+
+    public Money calculatePrice(Rental rental) {
+        Money totalPrice = rental.getPricePerMinute().multipliedBy(rental.getRentalDuration().durationInMinutes());
+        if(rental.getReservationDuration().durationInMinutes() - rental.getBaseMinutesToReach().durationInMinutes() > 0) {
+            Duration paidExtensionTime = Duration.ofMinutes(rental.getReservationDuration().durationInMinutes() - rental.getBaseMinutesToReach().durationInMinutes());
+            totalPrice = totalPrice.plus(rental.getExtensionCostPerMinute().multipliedBy(paidExtensionTime.durationInMinutes()));
+        }
+        int extraKilometers = rental.getKilometersDriven() - rental.getBaseKilometers();
+        if(extraKilometers > 0) {
+            totalPrice = totalPrice.plus(rental.getExtendedKilometerPrice().multipliedBy(extraKilometers));
+        }
+        return totalPrice;
+    }
 }
--- src/test/java/com/carriesCars/pricingEngine/PricingEngineTest.java
+++ src/test/java/com/carriesCars/pricingEngine/PricingEngineTest.java
@@ -20,4 +20,92 @@ public class PricingEngineTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void CalculatePrice_extended_reservation() {
+        Duration baseMinutesToReach = Duration.ofMinutes(20);
+        Duration reservationDuration = Duration.ofMinutes(50);
+        Duration drivingTime = Duration.ofMinutes(10);
+        Money drivingPricePerMinute = Money.of(CurrencyUnit.EUR, 0.24);
+        Money extensionPricePerMinute = Money.of(CurrencyUnit.EUR, 0.09);
+
+        PricingEngine pricingEngine = new PricingEngine();
+        Rental rental = new Rental();
+        rental.setReservationDuration(reservationDuration);
+        rental.setBaseMinutesToReach(baseMinutesToReach);
+        rental.setExtensionCostPerMinute(extensionPricePerMinute);
+
+        long extensionTime = reservationDuration.durationInMinutes() - baseMinutesToReach.durationInMinutes();
+        extensionTime = extensionTime >= 0 ? extensionTime : 0;
+        Money expected = extensionPricePerMinute.multipliedBy(extensionTime).plus(drivingPricePerMinute.multipliedBy(drivingTime.durationInMinutes()));
+        Money actual = pricingEngine.calculatePrice(drivingTime, drivingPricePerMinute, rental);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CalculatePrice_without_extension() {
+        Duration baseMinutesToReach = Duration.ofMinutes(20);
+        Duration reservationDuration = Duration.ofMinutes(10);
+        Duration drivingTime = Duration.ofMinutes(25);
+        Money drivingPricePerMinute = Money.of(CurrencyUnit.EUR, 0.24);
+        Money extensionPricePerMinute = Money.of(CurrencyUnit.EUR, 0.09);
+
+        PricingEngine pricingEngine = new PricingEngine();
+        Rental rental = new Rental();
+        rental.setReservationDuration(reservationDuration);
+        rental.setBaseMinutesToReach(baseMinutesToReach);
+        rental.setExtensionCostPerMinute(extensionPricePerMinute);
+
+        long extensionTime = reservationDuration.durationInMinutes() - baseMinutesToReach.durationInMinutes();
+        extensionTime = extensionTime >= 0 ? extensionTime : 0;
+        Money expected = extensionPricePerMinute.multipliedBy(extensionTime).plus(drivingPricePerMinute.multipliedBy(drivingTime.durationInMinutes()));
+        Money actual = pricingEngine.calculatePrice(drivingTime, drivingPricePerMinute, rental);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CalculatePrice_kilometer_extension() {
+        Duration baseMinutesToReach = Duration.ofMinutes(20);
+        Duration reservationDuration = Duration.ofMinutes(47);
+        Duration rentalTime = Duration.ofMinutes(135);
+        Money drivingPricePerMinute = Money.of(CurrencyUnit.EUR, 0.24);
+        Money extensionPricePerMinute = Money.of(CurrencyUnit.EUR, 0.09);
+        Money extendedKilometerPrice = Money.of(CurrencyUnit.EUR, 0.19);
+        int baseKilometers = 250;
+        int kilometersDriven = 287;
+
+        PricingEngine pricingEngine = new PricingEngine();
+        Rental rental = new Rental();
+        rental.setRentalDuration(rentalTime);
+        rental.setReservationDuration(reservationDuration);
+        rental.setBaseMinutesToReach(baseMinutesToReach);
+        rental.setExtensionCostPerMinute(extensionPricePerMinute);
+        rental.setPricePerMinute(drivingPricePerMinute);
+        rental.setExtendedKilometerPrice(extendedKilometerPrice);
+        rental.setBaseKilometers(baseKilometers);
+        rental.setKilometersDriven(kilometersDriven);
+
+        Money expected = Money.of(CurrencyUnit.EUR, 0);
+        expected = expected.plus(drivingPricePerMinute.multipliedBy(rentalTime.durationInMinutes()));
+        expected = expected.plus(extendedKilometerPrice.multipliedBy(kilometersDriven - baseKilometers));
+        expected = expected.plus(extensionPricePerMinute.multipliedBy(reservationDuration.durationInMinutes() - baseMinutesToReach.durationInMinutes()));
+
+        Money actual = pricingEngine.calculatePrice(rental);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CalculatePrice_default_package() {
+        Package defaultPackage = Package.defaultPackage();
+        Rental rental = new Rental();
+        rental.setAPackage(defaultPackage);
+        rental.setRentalDuration(Duration.ofMinutes(123));
+        rental.setKilometersDriven(189);
+    }
+
+    @Test
+    public void CalculatePrice_driving_price() {
+        Duration drivingDuration = Duration.ofMinutes(123);
+        PricingEngine.calculate
+    }
+
 }
